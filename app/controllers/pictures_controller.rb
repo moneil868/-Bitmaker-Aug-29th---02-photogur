0f2f36631fef747d74d33d9c429baf5502f21703
Marlon O'Neil <hello@marlononeil.com>
2017-08-29 14:53:02 -0400

    added ability to edit pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,32 +1,51 @@
 class PicturesController < ApplicationController
 
-def index
-  @pictures = Picture.all
-end
-
-def show
-  @picture = Picture.find(params[:id])
-end
-
-def new
-  @picture = Picture.new
-end
-
-def create
-  @picture = Picture.new
-
-  @picture.title = params[:picture][:title]
-  @picture.artist = params[:picture][:artist]
-  @picture.url = params[:picture][:url]
-
-
-  if @picture.save
-    # if the picture gets saved, generate a get request to "/pictures" (the index)
-    redirect_to "/pictures"
-  else
-    # otherwise render new.html.erb
-    render :new
+  def index
+    @pictures = Picture.all
   end
-end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.new
+
+    @picture.title = params[:picture][:title]
+    @picture.artist = params[:picture][:artist]
+    @picture.url = params[:picture][:url]
+
+
+    if @picture.save
+      # if the picture gets saved, generate a get request to "/pictures" (the index)
+      redirect_to "/pictures"
+    else
+      # otherwise render new.html.erb
+      render :new
+    end
+  end
+
+  def edit
+  @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+
+    @picture.title = params[:picture][:title]
+    @picture.artist = params[:picture][:artist]
+    @picture.url = params[:picture][:url]
+
+
+    if @picture.save
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
+    end
+  end
 
 end
