143f2077134d301e4965d0c9827843ccb39960a5
Marlon O'Neil <hello@marlononeil.com>
2017-09-05 15:46:41 -0400

    completed displaying validations on Picture add view

--- FIRST_RUN/app/models/picture.rb
+++ FIRST_RUN/app/models/picture.rb
@@ -1,13 +1,11 @@
 class Picture < ActiveRecord::Base
 
   validates :artist, :url, presence: true
-
   validates :title, length: { maximum: 20,
     too_long: "%{count} characters is the maximum allowed" }
   validates :title, length: { minimum: 3,
     too_short: "%{count} characters is the minimum allowed" }
-
-    validates :url, uniqueness: true
+  validates :url, uniqueness: true
 
   def self.newest_first
     Picture.order("created_at DESC")
