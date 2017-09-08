84ea95dd2a87ca95c6508ff61b308b08c9fcd47e
Marlon O'Neil <hello@marlononeil.com>
2017-09-08 16:01:46 -0400

    created User model and migrated database

--- FIRST_RUN/db/schema.rb
+++ FIRST_RUN/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170829203345) do
+ActiveRecord::Schema.define(version: 20170908200039) do
 
   create_table "pictures", force: :cascade do |t|
     t.string   "artist"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20170829203345) do
     t.date     "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
