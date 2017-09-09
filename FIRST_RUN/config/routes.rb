b97662059a0d34afdff6a9f4bea41b9905d3347f
Marlon O'Neil <hello@marlononeil.com>
2017-09-09 13:15:47 -0400

    completed authentication

--- FIRST_RUN/config/routes.rb
+++ FIRST_RUN/config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-
-  get 'sessions/create'
-
   root 'pictures#index'
   resource :users, only: %i(create new)
   resource :sessions, only: %i(create new destroy)
