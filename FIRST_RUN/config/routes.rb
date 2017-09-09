2371b27ed4a34515b12223cd31584b38539b44be
Marlon O'Neil <hello@marlononeil.com>
2017-09-09 12:58:56 -0400

    completed create new user form

--- FIRST_RUN/config/routes.rb
+++ FIRST_RUN/config/routes.rb
@@ -1,25 +1,9 @@
 Rails.application.routes.draw do
+
   root 'pictures#index'
-   get 'pictures' => 'pictures#index'
-
-  post 'pictures' => 'pictures#create'
-  get 'pictures/new' => 'pictures#new'
-
-  get 'pictures/:id' => 'pictures#show'
-
-  get 'pictures/:id/edit' => "pictures#edit"
-  patch 'pictures/:id' => "pictures#update"
-
-  delete 'pictures/:id' => 'pictures#destroy'
-
-
-  get 'posts'          => 'posts#index'
-  post 'posts'         => 'posts#create'
-  get 'posts/new'      => 'posts#new'
-  get 'posts/:id'      => 'posts#show'
-  get 'posts/:id/edit' => "posts#edit"
-  patch 'posts/:id'    => "posts#update"
-  delete 'posts/:id'   => 'posts#destroy'
+  resource :users, only: %i(create new)
+  resources :pictures
+  resources :posts
 
 
 end
