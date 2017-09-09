6dc9c9210216bec6730132d71446467c24044381
Marlon O'Neil <hello@marlononeil.com>
2017-09-09 13:01:26 -0400

    created sessions controller and views

--- FIRST_RUN/config/routes.rb
+++ FIRST_RUN/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  get 'sessions/create'
+
   root 'pictures#index'
   resource :users, only: %i(create new)
+  resource :sessions, only: %i(create new destroy)
   resources :pictures
   resources :posts
 
